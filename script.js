87a446a9b3a7f12c88a65daa490eabc0d8d06a45
Yogi Pradnyana <15tkj1yogipradnyana.com>
2022-03-16 22:33:44 +0800

    Menambahkan function toggle() & class done pada css

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ function add() {
   // 1. Mengambil nilai dari teksnya
   let newText = document.getElementById('new_text');
   // 2. Tambahkan list baru pada ul
-  let newTodo = '<li> <span>' + newText.value + '<span> [x] </span>' + '</span> </li>';
+  let newTodo = "<li> <span onclick='toggle(this)'>" + newText.value + '</span>' + '<span> [x] </span>' + ' </li>';
   todo.insertAdjacentHTML('afterbegin', newTodo);
   // 3. Kosongkan fieldnya
   newText.value = '';
+}
+
+function toggle(el) {
+  el.classList.toggle('done');
 }
